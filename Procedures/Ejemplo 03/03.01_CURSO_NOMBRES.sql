cd859e1bca0c6e2372a8029f8460b696eef54938
ManuelGC98 <32265620+ManuelGC98@users.noreply.github.com>
2018-05-17 14:01:27 +0200

    Add files via upload

---
+++ Procedures/Ejemplo 03/03.01_CURSO_NOMBRES.sql
@@ -0,0 +1,825 @@
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE CURSO_NOMBRES';
+EXCEPTION 
+   WHEN OTHERS THEN NULL;
+END;
+/
+
+
+CREATE TABLE CURSO_NOMBRES
+(
+  NOMBRE  VARCHAR2(128)
+);
+/
+
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('HUGO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('DANIEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('PABLO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARTIN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALEJANDRO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ADRIAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALVARO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('DAVID');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LUCAS');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARIO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('DIEGO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MANUEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LEO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MATEO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JAVIER');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('IZAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARCOS');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALEX');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('SERGIO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARC');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CARLOS');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JORGE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MIGUEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ENZO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ANTONIO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ANGEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('GONZALO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('IKER');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JUAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ERIC');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('IVAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('RUBEN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('VICTOR');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NICOLAS');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('BRUNO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('SAMUEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('HECTOR');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JOSE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('GABRIEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('DARIO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('OLIVER');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AARON');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ADAM');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('DYLAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JESUS');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARCO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AITOR');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALBERTO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('GUILLERMO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('RAUL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('RODRIGO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('FRANCISCO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JOEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ERIK');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('PAU');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('PEDRO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LUIS');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JAIME');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('RAFAEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ASIER');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('UNAI');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MOHAMED');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARTI');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('GAEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('THIAGO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('IAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('FERNANDO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('OSCAR');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LUCA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ANDRES');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('BIEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ISMAEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALONSO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('POL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NIL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('RAYAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALEIX');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ARNAU');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CRISTIAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('SAUL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ISAAC');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('SANTIAGO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JULEN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JOAN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MIGUEL ANGEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AIMAR');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('IGNACIO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('YOUSSEF');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('EDUARDO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MAURO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ENRIQUE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('YAGO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JOSE ANTONIO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('GERARD');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ABRAHAM');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NOAH');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('OMAR');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('IBAI');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('FRANCISCO JAVIER');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JON');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LUCIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARTINA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('PAULA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('SOFIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('DANIELA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALBA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JULIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CARLA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('SARA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('VALERIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NOA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('EMMA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CLAUDIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CARMEN');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARTA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('VALENTINA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('IRENE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ADRIANA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ANA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LAURA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ELENA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALEJANDRA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('OLIVIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('INES');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LOLA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AITANA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ARIADNA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('VERA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LAIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('VEGA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALMA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CANDELA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARINA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AINHOA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ELSA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JIMENA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('TRIANA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LEIRE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CARLOTA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('BLANCA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ROCIO');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AINARA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALICIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NORA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NEREA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ANGELA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CLARA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ABRIL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LEYRE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CELIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ANDREA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('VICTORIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LARA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('EVA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CHLOE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NATALIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MANUELA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ISABEL');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AINA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ZOE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CRISTINA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AROA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NURIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ALEXIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CAROLINA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('IRIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AFRICA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MAR');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MARA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('GABRIELA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ONA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NAIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('HELENA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('PAOLA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AZAHARA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NOELIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ELIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('IRIS');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('LUNA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MALAK');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NAHIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('BERTA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('AYA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MIREIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('SALMA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ANE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CAYETANA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('SILVIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('DIANA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('MIRIAM');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ARLET');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NADIA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NAIARA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('JANA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('CLOE');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ANNA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('NAYARA');
+Insert into CURSO_NOMBRES
+   (NOMBRE)
+ Values
+   ('ERIKA');
+COMMIT;
+/
+
